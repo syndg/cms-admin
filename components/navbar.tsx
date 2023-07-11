8f850408e43156d0eb0bd2a7babc1d90d76f2648
Sinchan Dasgupta <sinchandsgpt9@gmail.com>
2023-07-12 03:50:00 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className="sticky top-0 dark:bg-slate-950">
+    <header className="sticky z-30 top-0 dark:bg-slate-950">
       <nav className="flex items-center justify-between px-4 py-3 max-w-[1400px] mx-auto">
         <Link href="/" className="font-bold text-2xl">
           Dashboard
